bed8772c5c7ff902dd773e77280b8211459e1a6a
Takuma Iida <ta9maxxx@gmail.com>
2018-06-17 03:15:02 +0900

    UserKekka

--- karaoke_iOS/UserKekka/UserKekka.swift
+++ karaoke_iOS/UserKekka/UserKekka.swift
@@ -8,17 +8,64 @@
 
 import UIKit
 
-class UserKekka: UIViewController {
+class UserKekka: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableA: UITableView!
+    
+    @IBOutlet weak var tableB: UITableView!
+    
+    @IBOutlet weak var tableC: UITableView!
+    let cellName = "cellName"
+    
+    let roomA_Array:Array = [
+        "sample1","sample2"
+        //APIから帰ってきたチームAの配列
+    ]
+    
+    let roomB_Array:Array = [
+        "sample1","sample2"
+        //APIから帰ってきたチームBの配列
+    ]
+    
+    let roomC_Array:Array = [
+        "sample1","sample2"
+        //APIから帰ってきたチームCの配列
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        tableA.register(UITableViewCell.self, forCellReuseIdentifier: cellName)
+        tableB.register(UITableViewCell.self, forCellReuseIdentifier: cellName)
+        tableC.register(UITableViewCell.self, forCellReuseIdentifier: cellName)
+        //delegate、dataSourceはStoryBoardで設定した(たくちゃん)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch tableView.tag { //上からtableViewの順番で対応
+        case 0:
+            return 0
+            
+        case 1:
+            return 0
+            
+        case 2:
+            return 0
+            
+        default:
+            return 0
+        }
+    }
+
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellName, for: indexPath) as UITableViewCell
+        return cell
     }
     
 
