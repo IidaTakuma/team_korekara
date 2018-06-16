bb72fa625fc04502d8a318d43a6f5aef0caa2880
Takuma Iida <ta9maxxx@gmail.com>
2018-06-16 16:33:36 +0900

    gitignore設定

---
+++ karaoke_iOS/karaoke_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  karaoke_iOS
+//
+//  Created by 飯田拓馬 on 2018/06/16.
+//  Copyright © 2018年 CATK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
