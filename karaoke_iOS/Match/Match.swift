619721f7211bf08979017afa3a2a3de9fe68a128
oyoyoa <omim4841@gmail.com>
2018-06-17 05:46:28 +0900

    graphの表示

--- karaoke_iOS/Match/Match.swift
+++ karaoke_iOS/Match/Match.swift
@@ -7,22 +7,42 @@
 //
 
 import UIKit
+import Charts
+
 
 class Match: UIViewController {
     @IBOutlet weak var userName: UILabel!
     @IBOutlet weak var percent: UILabel!
     
+    let chartview:PieChartView = PieChartView(frame: CGRect(x: 67, y: 200, width: 250, height: 250))
+    
     var matchGraph:Int = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        srand48(0)
+        
+        chartview.highlightPerTapEnabled = false
+        chartview.chartDescription?.enabled = false
+        chartview.legend.enabled = false
+        chartview.rotationEnabled = false
+        
+        //1つ目のvalueにマッチ度の値をいれる。二つ目は100から1つ目のvalueを引く
+        let set = PieChartDataSet(values: [PieChartDataEntry(value: 80),PieChartDataEntry(value: 20)], label: "Election Results")
+        set.setColors(randomColor(),UIColor.white)
+        chartview.data = PieChartData(dataSet: set)
+        self.view.addSubview(chartview)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
 
     }
-
+    func randomColor() -> UIColor{
+        return UIColor(red: CGFloat(drand48()),
+                       green: CGFloat(drand48()),
+                       blue: CGFloat(drand48()),
+                       alpha: 1.0)
+    }
 
 }
