cc9af590af80a882e6613adf5f3b1447b07d48ec
hanachanm <aya.yb.0427@gmail.com>
2018-06-17 07:52:20 +0900

    WIPtotyu_uchdia

--- karaoke_iOS/KanjiStart/KanjiStart.swift
+++ karaoke_iOS/KanjiStart/KanjiStart.swift
@@ -8,21 +8,52 @@
 
 import UIKit
 
-class KanjiStart: UIViewController {
-
-    @IBOutlet weak var roomId: UILabel!
-    @IBOutlet weak var password: UILabel!
+class KanjiStart: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource   {
+    var sepSelect = ["ランダム","アーティスト"]
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+    return sepSelect.count
+    }
+    // UIPickerViewの列の数
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+    }
+    // UIPickerViewの行数、要素の全数
+    func pickerView(_ pickerView: UIPickerView,
+    numberOfRowsInComponent component: Int) -> Int {
+    return sepSelect.count
+    }
     
-    @IBOutlet weak var omakase: UIButton!
-    @IBOutlet weak var ganre: UIButton!
-    @IBOutlet weak var artist: UIButton!
+    // UIPickerViewに表示する配列
+    func pickerView(_ pickerView: UIPickerView,
+    titleForRow row: Int,
+    forComponent component: Int) -> String? {
     
-    @IBOutlet weak var start: UIButton!
+    return sepSelect[row]
+    }
+    
+    // UIPickerViewのRowが選択された時の挙動
+    func pickerView(_ pickerView: UIPickerView,
+    didSelectRow row: Int,
+    inComponent component: Int) {
+    // 処理
+    //artists[row]で値をとる
+    print("取り出し" + String(sepSelect[row]))
+        
+    }
+    @IBOutlet weak var roomKakuninId: UILabel!
+    @IBOutlet weak var kakuninPassword: UILabel!
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        picker.showsSelectionIndicator = true
+        picker.dataSource = self
+        picker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,5 +71,5 @@ class KanjiStart: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    }
 
-}
