fbc30cccad38371c8a020ff2f8afee52b8f82cc2
oyoyoa <omim4841@gmail.com>
2018-06-17 09:03:19 +0900

    見た目

---
+++ karaoke_iOS/karaoke_iOS/MainTabBarController.swift
@@ -0,0 +1,45 @@
+//
+//  MainTabBarController.swift
+//  karaoke_iOS
+//
+//  Created by や on 2018/06/17.
+//  Copyright © 2018年 CATK. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.selectedIndex = 0
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        switch item.tag {
+        case 0:
+            self.tabBar.barTintColor = UIColor(red: 1, green: 0.83, blue: 0.39, alpha: 1)
+            print("新規幹事")
+            break
+            
+        case 1:
+            print("マイページ")
+            break
+            
+        case 2:
+            print("入手")
+            break
+        default:
+            break
+        }
+    }
+
+
+}
